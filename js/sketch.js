a6a1665ef550decb07be67e8e6a0dc27c3183c5d
Robert Klim <kontakt.robert.klim@gmail.com>
2018-04-20 10:16:06 +0200

    Add basic game functionality (bird, pipes, jumping, collision detection)

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,41 @@
 // Flappy bird
 
+let bird;
+let pipes = [];
+
 function setup() {
-    createCanvas(431, 431);
+    createCanvas(400, 600);
+    bird = new Bird();
+    pipes.push(new Pipe());
 }
 
 function draw() {
     background(0);
 
+    for (let i = pipes.length-1; i >= 0; i--) {
+        pipes[i].show();
+        pipes[i].update();
+
+        if (pipes[i].hits(bird)) {
+            console.log("HIT!");
+        }
+
+        if (pipes[i].offscreen()) {
+            pipes.splice(i, 1);
+        }
+    }
+
+    bird.show();
+    bird.update();
+
+    if (frameCount % 100 == 0) {
+        pipes.push(new Pipe());
+    }
+
+}
+
+function keyPressed() {
+    if (key == ' ') {
+        bird.up();
+    }
 }
